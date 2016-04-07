f0575932160497086130e60c437c054e95310127
Kevin Chou <k29645806@gmail.com>
2016-04-07 23:37:56 +0800

    New check_connection but not yet pass

--- home/check_connection.py
+++ home/check_connection.py
@@ -70,29 +70,63 @@ print check_connection(network_1,first_1,second_1)
 
 
 
+#----------------------------------------------------------------------------#
 '''
-network_test = [set([1,2]), set([3,4]), set([5,6])]
-print groupTogether(network_test)
-print cvtNetwork(network)
-'''
-'''
-for conn in network:
-    index = 0
-    while True:
-        try:
-            inGroup, newGroup = checkIfConnected(conn,group[index])
-            if inGroup:
-                group[index] = newGroup
+network = ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super")
+first = "scout2"
+second = "scout3"
+
+network_1 = ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+     "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super")
+first_1 = "dr101"
+second_1 = "sscout"
+
+network_2 = ("a-b","c-d","b-c")
+first_2 = "a"
+second_2 = "d"
+
+def check_connection(network, first, second):
+
+    def checkIfConnected(connection,group):
+        connection, group = set(connection), set(group)
+        inGroup = group.intersection(connection)
+        if inGroup:
+            new_group = group | connection
+            return True, new_group
+        else:
+            return False, group
+
+    network_list = []
+    for conn in network:
+        network_list.append(conn.split('-'))
+    print network_list,'\n'
+
+    group = {}
+    for conn in network_list:
+        index = 0
+        while True:
+            try:
+                inGroup, newGroup = checkIfConnected(conn,group[index])
+                if inGroup:
+                    group[index] = newGroup
+                    break
+                else:
+                    index += 1
+            except KeyError:
+                group[index] = conn
                 break
-            else:
-                index += 1
-        except KeyError:
-            group[index] = conn
+    print group
+    # Check subGroup if there is connection
+
+
+
+
+    for subGroup in group.values():
+        if (first in subGroup) and (second in subGroup):
+            return True
             break
-'''
-'''
-a = [55,66,77,88,99]
-for i,x in enumerate(a):
-    for y in a[i+1::]:
-        print x, y
+    return False
+
+print check_connection(network_2, first_2, second_2)
 '''
